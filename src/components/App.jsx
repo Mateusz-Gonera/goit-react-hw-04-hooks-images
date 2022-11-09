f8167fb61afe585e91520ca29a9323d98672eb49
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-11-09 23:15:53 +0100

    fetch begin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchImages } from './Api/Api';
 import { Loader } from './Loader/Loader';
 import { Searchbar } from './Searchbar/Searchbar';
@@ -6,115 +6,147 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { Modal } from './Modal/Modal';
 import { Button } from './Button/Button';
+import axios from 'axios';
 
-const INITIAL_STATE = {
-  images: [],
-  error: null,
-  isLoading: false,
-  search: '',
-  isModalOpen: false,
-  largeImage: '',
-  page: 1,
+// const INITIAL_STATE = {
+//   images: [],
+//   error: null,
+//   isLoading: false,
+//   search: '',
+//   isModalOpen: false,
+//   largeImage: '',
+//   page: 1,
+// };
+
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [largeImage, setLargeImage] = useState('');
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchImages = async (query, page, perPage) => {
+      const result = await axios.get(
+        `https://pixabay.com/api/?key=29396920-d4426056c3f6851287cd3980f&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=${perPage}&safesearch=true`
+      );
+      setImages(oldImages => [...result.data.hits]);
+      console.log(result.data.hits);
+    };
+    fetchImages('cat', page, 12);
+  }, []);
+
+  // return (
+  //   <ImageGallery>
+  //     <ImageGalleryItem
+  //       images={images}
+  //       // onClick={this.handleImageClick}
+  //       loading={isLoading}
+  //     />
+  //   </ImageGallery>
+  // );
 };
 
-export class App extends Component {
-  state = { ...INITIAL_STATE };
+// export class App extends Component {
+//   state = { ...INITIAL_STATE };
 
-  handleSubmit = evt => {
-    evt.preventDefault();
+//   handleSubmit = evt => {
+//     evt.preventDefault();
 
-    const form = evt.currentTarget;
-    const input = form.elements.input.value;
-    this.setState({ images: [], search: input, page: 1 });
-    form.reset();
-  };
+//     const form = evt.currentTarget;
+//     const input = form.elements.input.value;
+//     this.setState({ images: [], search: input, page: 1 });
+//     form.reset();
+//   };
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.page !== this.state.page ||
-      prevState.search !== this.state.search
-    ) {
-      this.setState({ isLoading: true });
-      try {
-        const fetch = await fetchImages(this.state.search, this.state.page, 12);
-        this.setState(({ images }) => ({ images: [...images, ...fetch.hits] }));
-        document.addEventListener('keyup', e => {
-          if (e.key === 'Escape') {
-            this.closeModal();
-          }
-        });
-      } catch (error) {
-        console.log(error.message);
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
-  }
+//   async componentDidUpdate(prevProps, prevState) {
+//     if (
+//       prevState.page !== this.state.page ||
+//       prevState.search !== this.state.search
+//     ) {
+//       this.setState({ isLoading: true });
+//       try {
+//         const fetch = await fetchImages(this.state.search, this.state.page, 12);
+//         this.setState(({ images }) => ({ images: [...images, ...fetch.hits] }));
+//         document.addEventListener('keyup', e => {
+//           if (e.key === 'Escape') {
+//             this.closeModal();
+//           }
+//         });
+//       } catch (error) {
+//         console.log(error.message);
+//       } finally {
+//         this.setState({ isLoading: false });
+//       }
+//     }
+//   }
 
-  async componentDidMount() {
-    this.setState({ images: [], page: 1 });
-  }
+//   async componentDidMount() {
+//     this.setState({ images: [], page: 1 });
+//   }
 
-  componentWillUnmount() {
-    document.removeEventListener('keyup', e => {
-      if (e.key === 'Escape') {
-        this.closeModal();
-      }
-    });
-  }
+//   componentWillUnmount() {
+//     document.removeEventListener('keyup', e => {
+//       if (e.key === 'Escape') {
+//         this.closeModal();
+//       }
+//     });
+//   }
 
-  handleImageClick = imageID => {
-    const element = this.state.images.filter(image => {
-      return image.id === imageID;
-    });
-    const clickImg = element[0];
-    this.setState({ isModalOpen: true, largeImage: clickImg });
-  };
+//   handleImageClick = imageID => {
+//     const element = this.state.images.filter(image => {
+//       return image.id === imageID;
+//     });
+//     const clickImg = element[0];
+//     this.setState({ isModalOpen: true, largeImage: clickImg });
+//   };
 
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
-  };
+//   closeModal = () => {
+//     this.setState({ isModalOpen: false });
+//   };
 
-  loadMoreClick = () => {
-    this.setState({ isLoading: true });
-    try {
-      this.setState(({ page }) => ({ page: page + 1 }));
-    } catch (error) {
-      console.log(error.message);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
+//   loadMoreClick = () => {
+//     this.setState({ isLoading: true });
+//     try {
+//       this.setState(({ page }) => ({ page: page + 1 }));
+//     } catch (error) {
+//       console.log(error.message);
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
 
-  render() {
-    const { images, largeImage, isModalOpen, isLoading, page } = this.state;
-    return (
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr',
-          gridGap: '16px',
-          paddingBottom: '24px',
-        }}
-      >
-        {isModalOpen ? (
-          <Modal clickImage={largeImage} handleClose={this.closeModal} />
-        ) : null}
-        <Searchbar handleSubmit={this.handleSubmit} />
-        {isLoading & (page <= 1) ? <Loader /> : null}
-        <ImageGallery>
-          <ImageGalleryItem
-            images={images}
-            onClick={this.handleImageClick}
-            loading={isLoading}
-          />
-        </ImageGallery>
-        {isLoading & (page >= 2) ? <Loader /> : null}
+//   render() {
+//     const { images, largeImage, isModalOpen, isLoading, page } = this.state;
+//     return (
+//       <div
+//         style={{
+//           display: 'grid',
+//           gridTemplateColumns: '1fr',
+//           gridGap: '16px',
+//           paddingBottom: '24px',
+//         }}
+//       >
+//         {isModalOpen ? (
+//           <Modal clickImage={largeImage} handleClose={this.closeModal} />
+//         ) : null}
+//         <Searchbar handleSubmit={this.handleSubmit} />
+//         {isLoading & (page <= 1) ? <Loader /> : null}
+//         <ImageGallery>
+//           <ImageGalleryItem
+//             images={images}
+//             onClick={this.handleImageClick}
+//             loading={isLoading}
+//           />
+//         </ImageGallery>
+//         {isLoading & (page >= 2) ? <Loader /> : null}
 
-        {images.length === 0 ? null : (
-          <Button handleClick={this.loadMoreClick} />
-        )}
-      </div>
-    );
-  }
-}
+//         {images.length === 0 ? null : (
+//           <Button handleClick={this.loadMoreClick} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
